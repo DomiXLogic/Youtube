8a415b9e62db9f913bdbe1174abb8a59a505a756
Kyriakos Katsaras <katsaras.kyriakos@gmail.com>
2023-08-18 03:02:26 +0300

    Init commit

---
+++ GUI-Tutorials/README.md
@@ -0,0 +1,4 @@
+# Youtube Tutorial
+## _Java GUI_
+
+- ✨[Youtube Channel : @kkatsaras](https://www.youtube.com/channel/UCMclmj6LOpIpx7jIsfrsE7w)
---
+++ GUI-Tutorials/src/main/java/com/gui/tutorials/tabs/GUITutorials.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gui.tutorials.tabs;
+
+/**
+ *
+ * @author kyrka
+ */
+public class GUITutorials {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
